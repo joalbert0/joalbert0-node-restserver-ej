8ee3384e5fb4a1818765f32427e5ee410523151c
alberto <jorgealbertomancera@outlook.com>
2019-04-02 17:06:17 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,20 @@
 // ============================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// ============================
+//       base de datos
+// ============================
+
+let urlDB;
+if (process.env.NODE_ENV !== 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+
+    urlDB = 'mongodb+srv://hayabusaryu:ivnOXwd2v0H4vNX1@cluster0-ittoy.mongodb.net/cafe';
+
+
+
+}
+
+process.env.URLDB = urlDB;
